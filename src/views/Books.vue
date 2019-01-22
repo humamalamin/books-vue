a0868e3e870c3afae7b8368dfbf56d2ecc5d6973
humam al amin <humamalamin13@gmail.com>
2019-01-22 12:01:26 +0700

    initial setup

---
+++ src/views/Books.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="book">
+    <ApolloQuery :query="require('@/graphql/queries/Book.gql')" :variables="{ id: $route.params.id }">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+           <div>{{ data.book.title }}</div>
+           <div>{{ data.book.author }}</div>
+           <img :src="`http://localhost:8081/img/${data.book.image}`" alt="cover image">
+            <div>{{ data.book.description }}</div>
+        </div>
+      </template>
+    </ApolloQuery>
+  </div>
+</template>
